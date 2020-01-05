96439530497ab93c8fa3c7855da614ef572d8dff
Balamurali Arumugam <balamurali1601@gmail.com>
2020-01-05 15:51:18 +0530

    Aligning

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
-
+details:any
   constructor() { }
 
   ngOnInit() {
+
+    this.details = [
+    {'org':'Gavs Technologies','role':'Engineer','duration':'Sept 2019 - Till Date','domain':'Health Insurance'},
+    {'org':'Bankbazaar.com','role':'Integration Developer','duration':'Nov 2017 - Sept 2019','domain':'Fintech'},
+    {'org':'Excelacom Technologies','role':'Programmer Analyst','duration':'Jun 2016 - Nov 2017','domain':'Telecom'}
+    ]
   }
 
 }
